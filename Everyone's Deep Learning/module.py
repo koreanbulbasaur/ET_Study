acfb42d72e4ffd3c1d23e96421189782fea8709b
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-06-15 17:00:13 +0900

    upload

--- Everyone's Deep Learning/module.py
+++ Everyone's Deep Learning/module.py
@@ -1,19 +1,32 @@
 '''
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname('"../module.py"'))))
+import os, sys
+current_dir = os.path.dirname(os.path.abspath("../module.py"))
+sys.path.append(current_dir)
 from module import *
 '''
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# models
 Sequential = tf.keras.models.Sequential
+
+# layers
 Dense = tf.keras.layers.Dense
 LSTM = tf.keras.layers.LSTM
-to_categorical = tf.keras.utils.to_categorical
 Embedding = tf.keras.layers.Embedding
+Dropout = tf.keras.layers.Dropout
+Conv1D = tf.keras.layers.Conv1D
+MaxPooling1D = tf.keras.layers.MaxPooling1D
+Activation = tf.keras.layers.Activation
+
+# utils
+to_categorical = tf.keras.utils.to_categorical
+
+# preprocessing
 sequence = tf.keras.preprocessing.sequence
+
+# callbacks
 EarlyStopping = tf.keras.callbacks.EarlyStopping
 
 # 데이터
